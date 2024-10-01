cbff28b7152cd7496a85bd92d8275fb8af391e83
kjunior <164119125+k-junior12@users.noreply.github.com>
2024-10-01 14:59:31 -0500

    Brought out a new solution to the problem, had a little help here and there but will grasp everything as i move ahead

--- main.js
+++ main.js
@@ -1,71 +1,92 @@
-const choices = ["rock", "paper", "scissors"];
-const winners = [];
+game = ["rock","paper","scissor"]
 
-function game() {
-   for(let i = 0; i <=5; i++) {
-    PlayRound();
-   }
-   Whowins();
-    //play the game
-    //play five rounds
-    //console game
+// let the  computer choose between a random item from the game list
+function getComputerChoice() {
+    return game.at(Math.floor(Math.random() * 3))
 }
 
-function PlayRound() {
-const playerSelection = playerChoice();
-const computerSelection = computerChoice();
-const winner = checkWinner(playerSelection, computerSelection);
-winners.push(winner);
+// let the user choose between rock, paper or scissor
+ function getHumanChoice() {
+     let choice = prompt("choose ur pick rock, paper or scissor").toLowerCase()
 
+     if (choice === "rock" || choice === "paper" || choice === "scissor") {
+    alert("you choose " + choice.at(0).toUpperCase() + choice.slice(1))
+}   else {
+    alert("Invalid input, please only choose between rock, paper or scissor")
 }
-
-function playerChoice() {
-    //get input from player
-    let input = prompt("Type Rock, Paper, or Scissors");
-    while(input == null) {
-        input = prompt("Type Rock, Paper, Scissors");
-    }
-    input = input.toLowerCase();
-    let check = ValidateInput(input);
-    while (check == false) {
-        prompt("Enter only Rock, paper, or Scissors. ");
-        while(input == null) {
-            input = prompt("Type Rock, Paper, Scissors");
-        }
-        input = input.toLowerCase();
-        check = ValidateInput(input);
-    }
-    return input;
-
-}
-
-function computerChoice(){
-    //get random input from compute
-    //floor rounds down an interger*
-    return choices[Math.floor(Math.random()*choices.length)];
-}
-
-function ValidateInput(choice) {
- return choices.includes(choice);
- 
+    return choice
+    
  }
 
- function checkWinner(Choiceplayer, ChoiceComputer) {
-    if(Choiceplayer === ChoiceComputer) {
-        return "Its a Tie!";
-    } else if (
-        (Choiceplayer === "rock" && ChoiceComputer === "scissors") || 
-        (Choiceplayer === "paper" && ChoiceComputer === "rock") || 
-        (Choiceplayer === "scissors" && ChoiceComputer === "paper") 
-    ) {
-         return "Player Wins!";
+ //making the rules of the game
+
+ function playRound (humanChoice, computerChoice) {
+
+    if (humanChoice === computerChoice) {
+        alert("Its a tie!")
+        alert('Your score is '+ humanScore + ' and Computer Score is ' +computerScore)
+
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        alert("You win!  paper beats rock")
+        humanScore++
+        alert('Your score is '+humanScore+ ' and Computer Score is ' + computerScore)
+
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissors") {
+        alert("You lose!  scissors beats paper")
+        computerScore++
+        alert('Your score is '+humanScore+ ' and Computer Score is ' + computerScore)
+
+    }
+    else if (humanChoice === "rock" && computerChoice === "paper") {
+        alert("You lose! paper beats rock")
+        computerScore++
+        alert('Your score is '+humanScore+ ' and Computer Score is ' + computerScore)
+
+    }
+    
+    else if (humanChoice === "rock" && computerChoice === "scissors") {
+        alert("You win! rock beats scissors")
+        humanScore++
+        alert('Your score is '+humanScore+ ' and Computer Score is ' + computerScore)
+
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        alert("You win! scissors beats paper")
+        humanScore++ 
+        alert('Your score is '+humanScore+ ' and Computer Score is ' + computerScore)
+
+
+    }
+    else if (humanChoice === "scissors" && computerChoice === "rock") {
+        alert("You lose! rock beats scissors")
+        computerScore++
+        alert('Your score is '+humanScore+ ' and Computer Score is ' + computerScore)
+
+    }
+}
+    
+    
+
+
+function playGame() {
+    //making the score dashboard
+    humanScore = 0
+    computerScore = 0
+
+    //looping the function for 5times
+    for (let i=0; i <= 4; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection,computerSelection)
+    }
+    if (humanScore > computerScore) {
+        alert("Congratulations you won!")
     } else {
-        return "Computer Wins!";
+        alert("The computer won, better luck next time!")
     }
- }
+}
 
- function Whowins() {
-    console.log(winners);
- }
+playGame()
 
-game();
